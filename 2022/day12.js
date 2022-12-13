c0ab0daf201cef2d72050000ef894df6995efab1
Steve Khuu <steve.khuu@conductive.ai>
2022-12-13 17:30:55 -0500

    day12 part 1 fixed

--- 2022/day12.js
+++ 2022/day12.js
@@ -19,7 +19,7 @@ let maxWidth = 0;
 
 let shortestPath = {};
 
-async function getShortestPath() {
+async function parseGrid() {
   let currentY = 0;
 
   await fileUtil.fileReader('/day12_input.txt', (input) => {
@@ -40,63 +40,84 @@ async function getShortestPath() {
 
   maxHeight = grid.length;
   maxWidth = grid[0].length;
-  // console.log(grid);
 }
 
-function walkGrid(x, y, explored) {
-  let currentHeight = grid[y][x];
-  let updatedExplored = { ...explored };
-  updatedExplored[`${x},${y}`] = currentHeight;
+function walkGrid() {
+  let directionsToGo = [];
+  let visited = {};
 
-  if (currentHeight === END) {
-    let currentPathLength = Object.keys(shortestPath).length;
-    let newPathLength = Object.keys(updatedExplored).length;
+  directionsToGo.push({ x: startCoords[0], y: startCoords[1], steps: 0 });
 
-    if (currentPathLength === 0 || newPathLength < currentPathLength) {
-      shortestPath = { ...updatedExplored };
+  while (directionsToGo.length > 0) {
+    let { x, y, steps } = directionsToGo.shift();
+
+    let currentHeight = grid[y][x];
+
+    if (visited.hasOwnProperty(`${x},${y}`)) {
+      continue;
     }
-  } else {
-    let validDirections = [];
+
+    if (currentHeight == END) {
+      console.log(steps);
+      break;
+    }
 
     [].forEach.call(DIRECTIONS, function (direction) {
       let newCoordinates = [x + direction[0], y + direction[1]];
-      if (isAValidDirection(newCoordinates[0], newCoordinates[1], currentHeight, updatedExplored)) {
-        validDirections.push(newCoordinates);
+
+      if (isNotAValidDirection(newCoordinates[0], newCoordinates[1], currentHeight, visited)) {
+        return;
       }
+      directionsToGo.push({ x: newCoordinates[0], y: newCoordinates[1], steps: steps + 1 });
     });
 
-    [].forEach.call(validDirections, function (validDirection) {
-      let newX = validDirection[0];
-      let newY = validDirection[1];
-
-      // console.log(`${currentHeight} at (${x},${y}) will pursue:`);
-      // console.log(`     ${grid[newY][newX]} at (${newX},${newY})`);
-
-      walkGrid(newX, newY, updatedExplored);
-    });
+    let key = `${x},${y}`;
+    visited[key] = 1;
   }
+}
+
+function isNotAValidDirection(x, y, currentHeight, visited) {
+  return (
+    x < 0 ||
+    y < 0 ||
+    x == maxWidth ||
+    y == maxHeight ||
+    visited.hasOwnProperty(`${x},${y}`) ||
+    notValidNextCharacter(currentHeight, grid[y][x])
+  );
+}
+
+function notValidNextCharacter(current, next) {
+  let currentVal = current.charCodeAt(0);
+  if (current == START) {
+    currentVal = 'a'.charCodeAt(0);
+  }
+
+  let nextVal = next.charCodeAt(0);
+  if (next == END) {
+    nextVal = 'z'.charCodeAt(0);
+  }
+
+  return nextVal > currentVal + 1;
 }
 
 function isAValidDirection(x, y, currentHeight, explored) {
   return (
-    x >= 0 &&
-    x < maxWidth &&
-    y >= 0 &&
-    y < maxHeight &&
-    !explored.hasOwnProperty(`${x},${y}`) &&
-    (currentHeight === START ||
-      currentHeight.charCodeAt(0) + 1 === grid[y][x].charCodeAt(0) ||
-      currentHeight.charCodeAt(0) === grid[y][x].charCodeAt(0) ||
-      ((currentHeight == 'y' || currentHeight == 'z') && grid[y][x] === END))
+    (x >= 0 &&
+      x < maxWidth &&
+      y >= 0 &&
+      y < maxHeight &&
+      !explored.hasOwnProperty(`${x},${y}`) &&
+      (currentHeight.charCodeAt(0) + 1 === grid[y][x].charCodeAt(0) ||
+        currentHeight.charCodeAt(0) === grid[y][x].charCodeAt(0))) ||
+    (currentHeight == 'S' && grid[y][x] == 'a') ||
+    ((currentHeight == 'y' || currentHeight == 'z') && grid[y][x] === END)
   );
 }
 
 async function solve() {
-  await getShortestPath();
+  await parseGrid();
   walkGrid(startCoords[0], startCoords[1], {});
-  console.log('Shortest path found: ');
-  console.log(shortestPath);
-  console.log(Object.keys(shortestPath).length - 1);
 }
 
 solve();
