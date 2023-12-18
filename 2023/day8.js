9f5ac0534be7edb2655abc001b9d5221def8ef78
Stephen Khuu <skhuu@joinhomebase.com>
2023-12-18 16:31:36 -0500

    8B done

--- 2023/day8.js
+++ 2023/day8.js
@@ -7,20 +7,41 @@ let directions;
 let directionLength;
 let pathMap = {};
 
-const solve = () => {
-  let node = START;
-  let direction;
+let simultaneousStatus = [];
+
+const getLCM = (nums) => {
+  // If there's a faster way to do this, will revisit
+  let min = nums.reduce((acc, n) => Math.min(acc, n), nums[0]);
+
+  while (nums.some((n) => n % min != 0)) {
+    min++;
+  }
+
+  console.log(`The LCM is ${min}`);
+
+  return min;
+}
+
+const getPathCount = (s) => {
+  let node = s;
   let count = 0;
 
-  while(node != END) {
+  while(node.charAt(2) != 'Z') { // Refactored to work with 8A
     direction = directions.charAt(count % directionLength);
     node = pathMap[node][direction];
-
     count++;
   }
 
-  console.log(`DONE! ${count}`);
+  return count;
 };
+
+const solve8B = () => {
+  let count = 0;
+  let results = simultaneousStatus.map((s) => getPathCount(s));
+
+  getLCM(results);
+  console.log(`8B DONE! ${count}`);
+}
 
 async function day8() {
   await fileUtil.fileReader('/day8_input.txt', (input) => {
@@ -29,11 +50,15 @@ async function day8() {
       let pathContext = input.match(/\w{3}/g);
       if(pathContext) {
         let [path, left, right] = [...pathContext];
-
+        
         pathMap[path] = {
           'L': left,
           'R': right
         };
+
+        if (path.includes('A')){
+          simultaneousStatus.push(path);
+        }
       }
     } else {
       directions = input;
@@ -41,7 +66,9 @@ async function day8() {
     }
   });
 
-  solve();
+  console.log(`8A: ${getPathCount(START)}`);
+  solve8B();
 }
 
 day8();
+
