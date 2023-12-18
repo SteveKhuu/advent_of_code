e5271f18f08aca75bc86f580d3f3eba70999eab3
Stephen Khuu <skhuu@joinhomebase.com>
2023-12-17 22:34:07 -0500

    Day 5b hurts my brain

--- 2023/day5.js
+++ 2023/day5.js
@@ -4,7 +4,7 @@ class Range {
 
   constructor(destination, startingPoint, rangeCount) {
     this.mapping = this.mapping || {};
-
+    this.acceptableRange = {};
     this.updateRange(destination, startingPoint, rangeCount);  
   }
 
@@ -53,6 +53,22 @@ const getLowestLocation = (seeds, mappings) => {
   );
 };
 
+const getLowestLocationFromSeedRange = (seeds, mappings) => {
+  let [startingSeedNum, numSeeds] = [seeds.shift(), seeds.shift(), ...seeds];
+  let seedRange = Array(numSeeds).fill().map((seedNum, i) => startingSeedNum + i);
+
+  console.log(seedRange.length);
+  /*
+   * Day 5B: TBD, there's probably a way to do this without iterating through all generated seeds
+   * To think it though, we would just need to know the smallest mapping in each seed range
+   * 
+   * The smallest number can only be one of a few possibilities:
+   * - a number that falls through the mappings at some point
+   * - a nunber that falls through the smallest mapping
+   */
+  console.log(getLowestLocation(seedRange, mappings));
+};
+
 async function day5A() {
   let seeds = [];
   let mappings = [];
@@ -83,6 +99,7 @@ async function day5A() {
   });
 
   console.log(getLowestLocation(seeds, mappings));
+  getLowestLocationFromSeedRange(seeds, mappings);
 
 };
 
